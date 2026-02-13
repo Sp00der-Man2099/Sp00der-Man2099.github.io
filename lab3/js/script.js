ef5c244945f69d0b7649ce211abb134b11ab9663
Sp00der-Man2099 <adrikrenteria@gmail.com>
2026-02-12 17:50:15 -0800

    finished the lab 4

--- lab3/js/script.js
+++ lab3/js/script.js
@@ -2,7 +2,7 @@ document.querySelector("button").addEventListener("click", gradeQuiz);
 
 shuffleQ1Choices();
 function shuffleQ1Choices(){
-    let q1Choices = ["font-Color", "Color", "text-Color"];
+    let q1Choices = ["mew", "arceus", "bulbasaur"];
     q1Choices = _.shuffle(q1Choices);
     console.log(q1Choices);
     for(let i of q1Choices){
@@ -23,11 +23,52 @@ function shuffleQ1Choices(){
 
 function gradeQuiz(){
     let userAnswer1 = document.querySelector("input[name=q1]:checked").value;
-    alert("Grading Quiz..." + userAnswer1)
+    let userAnswer2 = document.querySelector("#q2").value
+    let userAnswer3 = document.querySelector("#dragonWeakness").value
+    let userAnswer4 = document.querySelector("#pokemonNum").value
+    let userAnswer5 = document.querySelector("input[name=q5]:checked").value
+    let score = 0;
+    //alert("Grading Quiz...")
 
-    if(userAnswer1 == "Color"){
-        //display CORRECT
+    if(userAnswer1 == "arceus"){
+        document.querySelector("#q1Text").style.backgroundColor = "lightgreen";
+        score+=20;
+    }else{
+        document.querySelector("#q1Text").style.backgroundColor = "lightcoral";
+    }
+
+    if(userAnswer2 == "Kanto"){
+        document.querySelector("#q2Text").style.backgroundColor = "lightgreen";
+        score+=20;
+    }else{
+        document.querySelector("#q2Text").style.backgroundColor = "lightcoral";
+    }
+
+    if(userAnswer3 == "Fairy"){
+        document.querySelector("#q3Text").style.backgroundColor = "lightgreen";
+        score+=20;
+    }else{
+        document.querySelector("#q3Text").style.backgroundColor = "lightcoral";
+    }
+
+    if(userAnswer4 == 151){
+        document.querySelector("#q4Text").style.backgroundColor = "lightgreen";
+         score+=20;
+    }else{
+        document.querySelector("#q4Text").style.backgroundColor = "lightcoral";
+    }
+
+    if(userAnswer5 == "Charmander"){
+        document.querySelector("#q5Text").style.backgroundColor = "lightgreen";
+         score+=20;
+    }else{
+        document.querySelector("#q5Text").style.backgroundColor = "lightcoral";
+    }
+
+     if(userAnswer1 == "arceus" && userAnswer2 == "Kanto" && userAnswer3 == "Fairy" && userAnswer4 == 151 && userAnswer5 == "Charmander"){
+        alert("Correct! Your score is " + score);
     } else{
-        //display WRONG
+        
+        alert("wrong! Your score is " + score);
     }
 }   
