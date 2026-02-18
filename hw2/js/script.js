a2a2dc3637ac174375a5f6c6336cb02ca7ab9065
Sp00der-Man2099 <adrikrenteria@gmail.com>
2026-02-18 15:19:03 -0800

    added some more functionality to my rock paper scissors game

--- hw2/js/script.js
+++ hw2/js/script.js
@@ -1,1 +1,28 @@
-alert("Hello!")
+document.querySelector("#chooseBtn").addEventListener("click", rpcGame);
+//made a list of possible choices for the player
+let choices = ["rock", "Rock", "ROCK",
+                "paper", "Paper", "PAPER",
+                "scissors", "Scissors", "SCISSORS"]
+//some fun secret choices
+let secretChoice = ["gun", "Gun, GUN"]
+
+
+function rpcGame(){
+    let playerChoice = document.querySelector("#playerChoice").value;
+    if(!choices.includes(playerChoice) && !secretChoice.includes(playerChoice)){
+        alert("🚨 INVALID CHOICE🚨 Its only three choices, cmon")
+        return;
+    }
+
+    //makes a random choice for the computer
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    //print it to console for testing
+    console.log(computerChoice);
+    if(secretChoice.includes(playerChoice)){
+        alert(("Damn, I forgot about that option. Congratulations Cowboy, you win!"))
+    }
+    
+
+
+
+}
